e40591b7e157c2551d2e057c4fde16a8647c208b
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-04-17 17:58:37 +0900

    Fix public path

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -71,6 +71,6 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.join(path.resolve(__dirname, '..'), 'out', 'dist'),
-        publicPath: '/',
+        publicPath: '/dist',
     },
 };
